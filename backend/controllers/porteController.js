0b9b7ebe575969235967ce0335ad2aeacb96e4de
chaima <chaima.bounaouara@gmail.com>
2026-05-19 14:58:42 +0100

    mise a jour

--- backend/controllers/porteController.js
+++ backend/controllers/porteController.js
@@ -53,7 +53,7 @@ const handleControlPorte = async (req, res) => {
             error: e.message,
           });
         }
-      }, 5000);
+      }, 7000);
     }
 
     console.log("[PORTE][API] Commande envoyee avec succes", {
